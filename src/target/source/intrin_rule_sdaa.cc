66db6641e534a76e21cad1d05b5f02bf65147d19
zly6 <zhanglinyuan5@163.com>
2023-03-29 20:32:35 +0800

    modify intrin_rule_sdaa.cc

--- src/target/source/intrin_rule_sdaa.cc
+++ src/target/source/intrin_rule_sdaa.cc
@@ -18,8 +18,8 @@
  */
 
 /*!
- * \file intrin_rule_cuda.cc
- * \brief CUDA intrinsic rules.
+ * \file intrin_rule_sdaa.cc
+ * \brief SDAA intrinsic rules.
  */
 #include <tvm/tir/builtin.h>
 #include <tvm/tir/op_attr_types.h>
@@ -29,10 +29,10 @@
 namespace tvm {
 namespace codegen {
 namespace intrin {
-// Add float suffix to the intrinsics, CUDA fast math.
+// Add float suffix to the intrinsics, SDAA fast math.
 using tir::FLowerIntrinsic;
 
-struct CUDAMath {
+struct SDAAMath {
   std::string operator()(DataType t, std::string name) const {
     if (t.is_float()) {
       switch (t.bits()) {
@@ -40,220 +40,223 @@ struct CUDAMath {
           return name;
         case 32:
           return name + 'f';
-        case 16: {
-          if (name == "fabs") {
-            return "__habs";
-          } else if (name == "round") {
-            return "hrint";
-          } else {
-            return "h" + name;
-          }
-        }
+        // zly: don't support half.
+        // case 16: {
+        //   if (name == "fabs") {
+        //     return "__habs";
+        //   } else if (name == "round") {
+        //     return "hrint";
+        //   } else {
+        //     return "h" + name;
+        //   }
+        // }
         default:
           return "";
       }
-    } else if (t.is_bfloat16()) {
-      return 'h' + name;
-    }
+    } // else if (t.is_bfloat16()) {
+    //   return 'h' + name;
+    // }
     return "";
   }
 };
 
-struct CUDAFastMath : public CUDAMath {
-  std::string operator()(DataType t, std::string name) const {
-    if (t.is_float() && t.bits() == 32) {
-      return "__" + name + 'f';
-    } else {
-      return CUDAMath::operator()(t, name);
-    }
-    return "";
-  }
-};
+// struct SDAAFastMath : public SDAAMath {
+//   std::string operator()(DataType t, std::string name) const {
+//     if (t.is_float() && t.bits() == 32) {
+//       return "__" + name + 'f';
+//     } else {
+//       return SDAAMath::operator()(t, name);
+//     }
+//     return "";
+//   }
+// };
 
-struct CUDAFastMathTan : public CUDAMath {
-  std::string operator()(DataType t, std::string name) const {
-    if (t.is_float()) {
-      switch (t.bits()) {
-        case 64:
-          return name;
-        // `__tanf` seems to produce some values too deviant from numpy tan version.
-        // So, let's use just `tanf` instead.
-        case 32:
-          return name + 'f';
-        case 16:
-          return 'h' + name;
-        default:
-          return "";
-      }
-    }
-    return "";
-  }
-};
+// struct SDAAFastMathTan : public SDAAMath {
+//   std::string operator()(DataType t, std::string name) const {
+//     if (t.is_float()) {
+//       switch (t.bits()) {
+//         case 64:
+//           return name;
+//         // `__tanf` seems to produce some values too deviant from numpy tan version.
+//         // So, let's use just `tanf` instead.
+//         case 32:
+//           return name + 'f';
+//         case 16:
+//           return 'h' + name;
+//         default:
+//           return "";
+//       }
+//     }
+//     return "";
+//   }
+// };
 
-struct CUDAPopcount {
-  std::string operator()(DataType t, std::string name) const {
-    if (t.is_uint()) {
-      switch (t.bits()) {
-        case 32:
-          return "__popc";
-        case 64:
-          return "__popcll";
-        default:
-          return "";
-      }
-    }
-    return "";
-  }
-};
+// struct SDAAPopcount {
+//   std::string operator()(DataType t, std::string name) const {
+//     if (t.is_uint()) {
+//       switch (t.bits()) {
+//         case 32:
+//           return "__popc";
+//         case 64:
+//           return "__popcll";
+//         default:
+//           return "";
+//       }
+//     }
+//     return "";
+//   }
+// };
 
-struct CUDAWarpIntrinsic {
-  const Op operator()(DataType t, const Op& orig_op) const {
-    if (orig_op.same_as(builtin::tvm_warp_shuffle())) {
-      return Op::Get("tir.cuda.__shfl_sync");
-    } else if (orig_op.same_as(builtin::tvm_warp_shuffle_up())) {
-      return Op::Get("tir.cuda.__shfl_up_sync");
-    } else {
-      ICHECK(orig_op.same_as(builtin::tvm_warp_shuffle_down()));
-      return Op::Get("tir.cuda.__shfl_down_sync");
-    }
-  }
-};
+// zly: sdaa don't have warp.
+// struct SDAAWarpIntrinsic {
+//   const Op operator()(DataType t, const Op& orig_op) const {
+//     if (orig_op.same_as(builtin::tvm_warp_shuffle())) {
+//       return Op::Get("tir.sdaa.__shfl_sync");
+//     } else if (orig_op.same_as(builtin::tvm_warp_shuffle_up())) {
+//       return Op::Get("tir.sdaa.__shfl_up_sync");
+//     } else {
+//       ICHECK(orig_op.same_as(builtin::tvm_warp_shuffle_down()));
+//       return Op::Get("tir.sdaa.__shfl_down_sync");
+//     }
+//   }
+// };
 
-static PrimExpr DispatchCUDAWarpActiveMask(const PrimExpr& e) {
-  const CallNode* call = e.as<CallNode>();
-  return Call(call->dtype, Op::Get("tir.cuda.__activemask"), call->args);
-}
+// static PrimExpr DispatchSDAAWarpActiveMask(const PrimExpr& e) {
+//   const CallNode* call = e.as<CallNode>();
+//   return Call(call->dtype, Op::Get("tir.sdaa.__activemask"), call->args);
+// }
 
-template <typename T>
-static PrimExpr DispatchCUDAShuffle(const PrimExpr& e) {
-  const CallNode* call = e.as<CallNode>();
-  ICHECK(call != nullptr);
-  ICHECK_EQ(call->args.size(), 5);  // mask, value, warp_id, width, warp_size
-  Array<PrimExpr> cuda_args{{call->args[0], call->args[1], call->args[2], call->args[3]}};
-  return Call(call->dtype, T()(call->dtype, Downcast<Op>(call->op)), cuda_args);
-}
+// template <typename T>
+// static PrimExpr DispatchSDAAShuffle(const PrimExpr& e) {
+//   const CallNode* call = e.as<CallNode>();
+//   ICHECK(call != nullptr);
+//   ICHECK_EQ(call->args.size(), 5);  // mask, value, warp_id, width, warp_size
+//   Array<PrimExpr> sdaa_args{{call->args[0], call->args[1], call->args[2], call->args[3]}};
+//   return Call(call->dtype, T()(call->dtype, Downcast<Op>(call->op)), sdaa_args);
+// }
 
-TVM_REGISTER_OP("tir.floor")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// zly: sdaa don't support based on the sdaa c programming guide.
+// TVM_REGISTER_OP("tir.floor")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.ceil")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.ceil")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.trunc")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.trunc")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.fabs")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.fabs")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.round")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.round")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.nearbyint")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.nearbyint")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.exp").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAFastMath>);
+TVM_REGISTER_OP("tir.exp").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.exp2")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.exp2")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.exp10")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAFastMath>);
+// TVM_REGISTER_OP("tir.exp10")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAFastMath>);
 
-TVM_REGISTER_OP("tir.erf").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAMath>);
+TVM_REGISTER_OP("tir.erf").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.log").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAFastMath>);
+TVM_REGISTER_OP("tir.log").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.log2")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAFastMath>);
+// TVM_REGISTER_OP("tir.log2")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAFastMath>);
 
-TVM_REGISTER_OP("tir.log10")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAFastMath>);
+// TVM_REGISTER_OP("tir.log10")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAFastMath>);
 
-TVM_REGISTER_OP("tir.tan").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAFastMathTan>);
+TVM_REGISTER_OP("tir.tan").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.cos").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAFastMath>);
+TVM_REGISTER_OP("tir.cos").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.cosh")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.cosh")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.sin").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAFastMath>);
+TVM_REGISTER_OP("tir.sin").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+                                                     DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.sinh")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.sinh")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.atan")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.atan")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
 TVM_REGISTER_OP("tir.tanh")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+    .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.sqrt")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.sqrt")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.pow").set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic",
-                                                     DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.pow").set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic",
+//                                                      DispatchPureExtern<SDAAMath>);
 
-TVM_REGISTER_OP("tir.popcount")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAPopcount>);
+// TVM_REGISTER_OP("tir.popcount")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAPopcount>);
 
-TVM_REGISTER_OP("tir.tvm_warp_shuffle")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchCUDAShuffle<CUDAWarpIntrinsic>);
+// TVM_REGISTER_OP("tir.tvm_warp_shuffle")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchSDAAShuffle<SDAAWarpIntrinsic>);
 
-TVM_REGISTER_OP("tir.tvm_warp_shuffle_up")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchCUDAShuffle<CUDAWarpIntrinsic>);
+// TVM_REGISTER_OP("tir.tvm_warp_shuffle_up")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchSDAAShuffle<SDAAWarpIntrinsic>);
 
-TVM_REGISTER_OP("tir.tvm_warp_shuffle_down")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchCUDAShuffle<CUDAWarpIntrinsic>);
+// TVM_REGISTER_OP("tir.tvm_warp_shuffle_down")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchSDAAShuffle<SDAAWarpIntrinsic>);
 
-TVM_REGISTER_OP("tir.tvm_warp_activemask")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchCUDAWarpActiveMask);
+// TVM_REGISTER_OP("tir.tvm_warp_activemask")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchSDAAWarpActiveMask);
 
-TVM_REGISTER_OP("tir.fmod")
-    .set_attr<FLowerIntrinsic>("cuda.FLowerIntrinsic", DispatchPureExtern<CUDAMath>);
+// TVM_REGISTER_OP("tir.fmod")
+//     .set_attr<FLowerIntrinsic>("sdaa.FLowerIntrinsic", DispatchPureExtern<SDAAMath>);
 
-// Register low-level builtin ops.
-// TODO(tvm-team): consider make CUDA its own subfolder and create a file for low-level builtins.
-TVM_REGISTER_OP("tir.cuda.__shfl_sync")
-    .set_num_inputs(4)
-    .add_argument("mask", "Expr", "The thread mask.")
-    .add_argument("var", "Expr", "The variable to sync.")
-    .add_argument("lane", "Expr", "The source thread id.")
-    .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
-    .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_sync")
-    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
-    .set_attr<bool>("cuda.need_warp_shuffle", true);
+// // Register low-level builtin ops.
+// // TODO(tvm-team): consider make SDAA its own subfolder and create a file for low-level builtins.
+// TVM_REGISTER_OP("tir.sdaa.__shfl_sync")
+//     .set_num_inputs(4)
+//     .add_argument("mask", "Expr", "The thread mask.")
+//     .add_argument("var", "Expr", "The variable to sync.")
+//     .add_argument("lane", "Expr", "The source thread id.")
+//     .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
+//     .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_sync")
+//     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
+//     .set_attr<bool>("sdaa.need_warp_shuffle", true);
 
-TVM_REGISTER_OP("tir.cuda.__shfl_up_sync")
-    .set_num_inputs(4)
-    .add_argument("mask", "Expr", "The thread mask.")
-    .add_argument("var", "Expr", "The variable to sync.")
-    .add_argument("delta", "Expr", "The source lane id offset to be added.")
-    .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
-    .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_up_sync")
-    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
-    .set_attr<bool>("cuda.need_warp_shuffle", true);
+// TVM_REGISTER_OP("tir.sdaa.__shfl_up_sync")
+//     .set_num_inputs(4)
+//     .add_argument("mask", "Expr", "The thread mask.")
+//     .add_argument("var", "Expr", "The variable to sync.")
+//     .add_argument("delta", "Expr", "The source lane id offset to be added.")
+//     .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
+//     .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_up_sync")
+//     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
+//     .set_attr<bool>("sdaa.need_warp_shuffle", true);
 
-TVM_REGISTER_OP("tir.cuda.__shfl_down_sync")
-    .set_num_inputs(4)
-    .add_argument("mask", "Expr", "The thread mask.")
-    .add_argument("var", "Expr", "The variable to sync.")
-    .add_argument("delta", "Expr", "The source lane id offset to be subtracted.")
-    .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
-    .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_down_sync")
-    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
-    .set_attr<bool>("cuda.need_warp_shuffle", true);
+// TVM_REGISTER_OP("tir.sdaa.__shfl_down_sync")
+//     .set_num_inputs(4)
+//     .add_argument("mask", "Expr", "The thread mask.")
+//     .add_argument("var", "Expr", "The variable to sync.")
+//     .add_argument("delta", "Expr", "The source lane id offset to be subtracted.")
+//     .add_argument("width", "Expr", "The warp thread width, must be a power of 2.")
+//     .set_attr<TGlobalSymbol>("TGlobalSymbol", "__shfl_down_sync")
+//     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kOpaque))
+//     .set_attr<bool>("sdaa.need_warp_shuffle", true);
 
-TVM_REGISTER_OP("tir.cuda.__activemask")
-    .set_num_inputs(0)
-    .set_attr<TGlobalSymbol>("TGlobalSymbol", "__activemask")
-    .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kPure))
-    .set_attr<bool>("cuda.need_warp_shuffle", true);
+// TVM_REGISTER_OP("tir.sdaa.__activemask")
+//     .set_num_inputs(0)
+//     .set_attr<TGlobalSymbol>("TGlobalSymbol", "__activemask")
+//     .set_attr<TCallEffectKind>("TCallEffectKind", Integer(CallEffectKind::kPure))
+//     .set_attr<bool>("sdaa.need_warp_shuffle", true);
 
 }  // namespace intrin
 }  // namespace codegen
